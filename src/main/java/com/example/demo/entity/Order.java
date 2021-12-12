944fd729aa11cf181e9a146bd996d5a3e794e07f
JSC0x62 <jscfalcons@gmail.com>
2021-12-11 20:06:49 -0600

    refactored classes

--- src/main/java/com/example/demo/entity/Order.java
+++ src/main/java/com/example/demo/entity/Order.java
@@ -9,44 +9,49 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-    @Entity
-    @Table(name="ORDERS")
-    public class Order
-    {
-        @Id @GeneratedValue(strategy=GenerationType.AUTO)
-        private Integer id;
-        @Column(nullable=false, name="cust_name")
-        private String customerName;
-        @Column(nullable=false, name="cust_email")
-        private String customerEmail;
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(nullable = false, name = "cust_name")
+    private String customerName;
+    @Column(nullable = false, name = "cust_email")
+    private String customerEmail;
 
-        @OneToMany(mappedBy="order")
-        private Set<OrderItem> orderItems;
+    @OneToMany(mappedBy = "order")
+    private Set<OrderItem> orderItems;
 
-        public Integer getId() {
-            return id;
-        }
-        public void setId(Integer id) {
-            this.id = id;
-        }
-        public String getCustomerName() {
-            return customerName;
-        }
-        public void setCustomerName(String customerName) {
-            this.customerName = customerName;
-        }
-        public String getCustomerEmail() {
-            return customerEmail;
-        }
-        public void setCustomerEmail(String customerEmail) {
-            this.customerEmail = customerEmail;
-        }
-        public Set<OrderItem> getOrderItems()
-        {
-            return orderItems;
-        }
-        public void setOrderItems(Set<OrderItem> orderItems)
-        {
-            this.orderItems = orderItems;
-        }
+    public Integer getId() {
+        return id;
     }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Set<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(Set<OrderItem> orderItems) {
+        this.orderItems = orderItems;
+    }
+}
--- src/main/java/com/example/demo/entity/OrderItem.java
+++ src/main/java/com/example/demo/entity/OrderItem.java
@@ -1,4 +1,5 @@
 package com.example.demo.entity;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,47 +10,46 @@ import javax.persistence.Table;
 
 
 @Entity
-@Table(name="ORDER_ITEMS")
-public class OrderItem
-{
-    @Id @GeneratedValue(strategy=GenerationType.AUTO)
+@Table(name = "ORDER_ITEMS")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String productCode;
     private int quantity;
     @ManyToOne
-    @JoinColumn(name="order_id")
+    @JoinColumn(name = "order_id")
     private Order order;
 
-    public Integer getId()
-    {
+    public Integer getId() {
         return id;
     }
-    public void setId(Integer id)
-    {
+
+    public void setId(Integer id) {
         this.id = id;
     }
-    public String getProductCode()
-    {
+
+    public String getProductCode() {
         return productCode;
     }
-    public void setProductCode(String productCode)
-    {
+
+    public void setProductCode(String productCode) {
         this.productCode = productCode;
     }
-    public int getQuantity()
-    {
+
+    public int getQuantity() {
         return quantity;
     }
-    public void setQuantity(int quantity)
-    {
+
+    public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
-    public Order getOrder()
-    {
+
+    public Order getOrder() {
         return order;
     }
-    public void setOrder(Order order)
-    {
+
+    public void setOrder(Order order) {
         this.order = order;
     }
 }
